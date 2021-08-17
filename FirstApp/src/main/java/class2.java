ea84cad5a3d36efe97807f4a8270278a0bc96200
Samiksha <Samiksha@LAPTOP-V5UKOA6H>
2021-08-17 10:50:27 +0530

    class 2 update

--- FirstApp/src/main/java/class2.java
+++ FirstApp/src/main/java/class2.java
@@ -4,4 +4,12 @@ public class class2 {
 	{
 		System.out.println("hello gm");
 	}
+	public void display() {
+		
+		int a=10;
+		int b=20;
+		int sum;
+		sum=a+b;
+		System.out.println("sum is"+sum);
+	}
 }
